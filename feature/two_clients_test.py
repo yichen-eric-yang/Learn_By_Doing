367793cfd196ee49abed7e951918b1377677f874
Eric <eric.yang@aspentech.com>
2020-12-09 12:49:18 +0800

    feat(two-clients): implement two clients testing

--- feature/two_clients_test.py
+++ feature/two_clients_test.py
@@ -30,12 +30,13 @@ class FIXMultiClient:
         """
         docstring
         """
-        msg1 = fixmsg({35: "6", 9: "97", 45: "6", 58: "first client"})
+        msg1 = fixmsg({35: "6", 15: "CNY", 45: "6", 58: "first client"})
         env.client1.send(msg1)
         exp_msg1 = fixmsg(
             {
                 8: "FIX.4.2",
-                9: "73",
+                9: "80",
+                15: "CNY",
                 35: "6",
                 45: "6",
                 49: env.client1.sender,
@@ -48,7 +49,28 @@ class FIXMultiClient:
             exp_msg1,
             received1,
             description="Message sent by client 1 match",
-            include_tags=[8, 9, 35, 45, 49, 56, 58],
+            include_tags=[8, 9, 15, 35, 45, 49, 56, 58],
+        )
+        msg2 = fixmsg({35: "D", 15: "EUR", 45: "16", 58: "second client"})
+        env.client2.send(msg2)
+        exp_msg2 = fixmsg(
+            {
+                8: "FIX.4.2",
+                9: "83",
+                15: "EUR",
+                35: "D",
+                45: "16",
+                49: env.client2.sender,
+                56: env.client2.target,
+                58: "second client",
+            }
+        )
+        received2 = env.server.receive((env.client2.target, env.client2.sender))
+        result.fix.match(
+            exp_msg2,
+            received2,
+            description="Message sent by client 2 match",
+            include_tags=[8, 9, 15, 35, 45, 49, 56, 58],
         )
 
 
@@ -66,6 +88,15 @@ def add_fix_test_two_clients():
                 host=context("server", "{{host}}"),
                 port=context("server", "{{port}}"),
                 sender="TW",
+                target="ISLD",
+                msgclass=FixMessage,
+                codec=codec,
+            ),
+            FixClient(
+                name="client2",
+                host=context("server", "{{host}}"),
+                port=context("server", "{{port}}"),
+                sender="TW2",
                 target="ISLD",
                 msgclass=FixMessage,
                 codec=codec,
